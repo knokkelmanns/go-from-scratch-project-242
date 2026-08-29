540acb8dd3c2db2ee91165fe56a5d75f14e96c87
knokkelmanns <knokkelmanns@gmail.com>
2026-08-29 15:30:26 +0200

    add urfave/cli v3

--- cmd/hexlet-path-size/main.go
+++ cmd/hexlet-path-size/main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/urfave/cli/v3"
+)
 
 func main() {
-	fmt.Println("Hello from Hexlet!")
+	cmd := &cli.Command{
+		Name:  "hexlet-path-size",
+		Usage: "анализатор размера диска",
+		Action: func(ctx context.Context, cmd *cli.Command) error {
+			fmt.Println("Hello from Hexlet!")
+			return nil
+		},
+	}
+
+	if err := cmd.Run(context.Background(), os.Args); err != nil {
+		log.Fatal(err)
+	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module code
 
 go 1.26.6
+
+require github.com/urfave/cli/v3 v3.11.0
