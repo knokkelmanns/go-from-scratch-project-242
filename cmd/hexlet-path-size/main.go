8850f03ffbef42f721ca872445e23d4266c72445
knokkelmanns <knokkelmanns@gmail.com>
2026-08-30 17:33:06 +0200

    read files and calculation size

--- cmd/hexlet-path-size/main.go
+++ cmd/hexlet-path-size/main.go
@@ -14,12 +14,43 @@ func main() {
 		Name:  "hexlet-path-size",
 		Usage: "анализатор размера диска",
 		Action: func(ctx context.Context, cmd *cli.Command) error {
-			fmt.Println("Hello from Hexlet!")
+			path := cmd.Args().First()
+			result, err := GetPathSize(path)
+			if err != nil {
+				return cli.Exit(err.Error(), 1)
+			}
+			fmt.Println(result + "\t" + path)
 			return nil
 		},
 	}
 
 	if err := cmd.Run(context.Background(), os.Args); err != nil {
 		log.Fatal(err)
+	}
+}
+
+func GetPathSize(path string) (string, error) {
+	info, err := os.Lstat(path)
+	if err != nil {
+		return "", err
+	}
+	if info.IsDir() {
+		entries, err := os.ReadDir(path)
+		if err != nil {
+			return "", err
+		}
+		var total int64
+		for _, entry := range entries {
+			entryInfo, err := entry.Info()
+			if err != nil {
+				return "", err
+			}
+			total += entryInfo.Size()
+		}
+		result := fmt.Sprintf("%dB", total)
+		return result, nil
+	} else {
+		result := fmt.Sprintf("%dB", info.Size())
+		return result, nil
 	}
 }
